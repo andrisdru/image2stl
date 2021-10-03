8aa33fd34d12cfdb84e38bd6ce6d68d6199a5050
andrisdru <andris@drulle.lv>
2021-10-03 18:26:02 +0300

    test

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ In general this script converts PNG or other image to STL file, by defaul white 
 
 1. Install python3 and its dependencies "pip3 install numpy-stl pillow argparse"
 1. Export from easyeda PNG image with 2.5x size
-2. run "python3 image2stl.py -i export.png -s 0.4" 
+2. run "python3 image2stl.py -i pcb.png -s 0.4" 
 
 * scale 0.4 because 1/2.5 = 0.4
 * pcb.png example file is not created by me, somewhere found it on internet
--- image2stl.py
+++ image2stl.py
@@ -4,18 +4,21 @@ import numpy as np
 from stl import mesh
 from PIL import Image
 
+
 parser = argparse.ArgumentParser(description='Converts image to STL for SLA Printers for PCB design')
 
 parser.add_argument("-s", "--scale", type=float, default=1, help="Scale, Size * Scale")
 parser.add_argument("-o", "--output", type=str, default='output.stl', help="output STL")
 parser.add_argument("-x", "--maxx", type=int, default=1440, help="Max X vertices")
 parser.add_argument("-y", "--maxy", type=int, default=2560, help="Max Y vertices")
+parser.add_argument("-f", "--offset", type=int, default=0, help="Offset for lines")
 parser.add_argument("-c", "--color", type=int, default=255, help="Color threshold, image is converted to greyscale where 0 white 255 black")
 requiredNamed = parser.add_argument_group('required named arguments')
 requiredNamed.add_argument("-i", "--input",required=True ,type=str, help="Input png, jpg, gif...")
 args = parser.parse_args()
 z=10 #height # set default height
 img = Image.open(args.input)
+offset = args.offset
 max_size=(args.maxx,args.maxy)
 color=args.color #0 white / 255 black
 
@@ -25,16 +28,57 @@ grey_img.thumbnail(max_size)
 imageNp = np.array(grey_img)
 (ncols,nrows)=grey_img.size
 
+def colored(pixel):
+  if pixel < color:
+    return True
+  else:
+    return False
+
+
+PIL_image = Image.fromarray(np.uint8(imageNp)).convert('L')
+PIL_image.save('img1.png')
+
+# setting offset basically making lines thinner 
+for i in range(offset):
+  for x in range(1, ncols-1):
+    for y in range(1, nrows-1): 
+      if colored(imageNp[y][x]):
+       if colored(imageNp[y-1][x]) and not colored(imageNp[y+1][x]): 
+          imageNp[y][x] = 255
+
+for i in range(offset):
+  for x in range(ncols-1, 1, -1):
+    for y in range(nrows-1, 1, -1):
+      if colored(imageNp[y][x]):
+        if colored(imageNp[y+1][x]) and not colored(imageNp[y-1][x]): 
+          imageNp[y][x] = 255
+
+
+for i in range(offset):
+  for x in range(1, ncols-1):
+    for y in range(1, nrows-1): 
+      if colored(imageNp[y][x]):
+       if colored(imageNp[y][x-1]) and not colored(imageNp[y][x+1]): 
+          imageNp[y][x] = 255
+
+for i in range(offset):
+  for x in range(ncols-1, 1, -1):
+    for y in range(nrows-1, 1, -1):
+      if colored(imageNp[y][x]):
+        if colored(imageNp[y][x+1]) and not colored(imageNp[y][x-1]): 
+          imageNp[y][x] = 255
+
+
 
 vertices=[]
 
 for x in range(0, ncols-1):
   for y in range(0, nrows-1):  
-    if imageNp[y][x] < color:
-      u = True if imageNp[y][x+1] >= color  else False 
-      r = True if imageNp[y+1][x] >= color  else False
-      d = True if imageNp[y][x-1] >= color  else False
-      l = True if imageNp[y-1][x] >= color  else False
+    if colored(imageNp[y][x]):
+      u = True if not colored(imageNp[y][x+1])  else False 
+      r = True if not colored(imageNp[y+1][x])  else False
+      d = True if not colored(imageNp[y][x-1])  else False
+      l = True if not colored(imageNp[y-1][x])  else False
       vertice=np.array([x, y, 0, u, r, d, l])
       vertices.append(vertice)
 
